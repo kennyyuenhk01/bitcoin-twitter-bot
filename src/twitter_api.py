becbdf4c39c41b6028157ef1069f9c9a11375130
Kenny Yuen <kennyyuen@KennydeMacBook-Pro.local>
2024-04-19 02:39:05 +0800

    fix: update twitter api setting

--- src/twitter_api.py
+++ src/twitter_api.py
@@ -2,33 +2,35 @@ import tweepy
 import os
 
 # 从环境变量中获取 Twitter 认证信息
+TWITTER_API_KEY = os.getenv('TWITTER_API_KEY')
+TWITTER_API_SECRET_KEY = os.getenv('TWITTER_API_SECRET_KEY')
+TWITTER_ACCESS_TOKEN = os.getenv('TWITTER_ACCESS_TOKEN')
+TWITTER_ACCESS_TOKEN_SECRET = os.getenv('TWITTER_ACCESS_TOKEN_SECRET')
 TWITTER_BEARER_TOKEN = os.getenv('TWITTER_BEARER_TOKEN')
 
 def authenticate_twitter():
     """
-    认证 Twitter API 并创建 API 客户端。
-    返回 tweepy.Client 对象。
+    认证 Twitter API 并创建 API 对象。
+    返回 tweepy API 对象。
     """
-    print("TWITTER_BEARER_TOKEN: ${TWITTER_BEARER_TOKEN}")
 
-    client = tweepy.Client(bearer_token=TWITTER_BEARER_TOKEN)
-    return client
+    # Gainaing access and connecting to Twitter API using Credentials
+    client = tweepy.Client(TWITTER_BEARER_TOKEN, TWITTER_API_KEY, TWITTER_API_SECRET_KEY, TWITTER_ACCESS_TOKEN, TWITTER_ACCESS_TOKEN_SECRET)
+
+    # Creating API instance. This is so we still have access to Twitter API V1 features
+    auth = tweepy.OAuth1UserHandler(TWITTER_API_KEY, TWITTER_API_SECRET_KEY, TWITTER_ACCESS_TOKEN, TWITTER_ACCESS_TOKEN_SECRET)
+    api = tweepy.API(auth)
+
+    return api
 
 def tweet_message(message):
     """
-    使用 Twitter API v2 发送一个推文。
+    发送一个推文。
     参数:
         message (str): 要发布的推文内容。
     返回:
-        tweepy.Response: 发送推文的响应对象。
+        tweepy.Status: 发送推文的状态对象。
     """
-    print("111")
-
-    client = authenticate_twitter()
-    response = client.create_tweet(text=message)
-    return response
-
-# 示例：发送推文
-if __name__ == "__main__":
-    result = tweet_message("Hello Twitter v2!")
-    print(result.data)  # 打印推文响应的详细信息
+    api = authenticate_twitter()
+    status = api.update_status(message)
+    return status
